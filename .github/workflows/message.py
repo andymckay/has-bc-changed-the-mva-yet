ccb2cc9299e6cecc7593687f76e87a5bc2db6ffc
Andy McKay <andymckay@github.com>
2021-04-11 19:24:40 -0700

    Update message.py

--- .github/workflows/message.py
+++ .github/workflows/message.py
@@ -1,4 +1,14 @@
 from datetime import date
+from random import choice
+
+suffix = [
+    "A lot has changed since then.",
+    "We are in a climate emergency.",
+    "It should be the Road User Act.",
+    "E-bikes are now a thing.",
+    "E-scooters and now a thing.",
+]
+
 # I don't know when in 1957 the BC MVA was written.
 created = date(1957, 1, 1)
 # Or when it was updated in 1996.
@@ -19,7 +29,8 @@ if day:
     elif day:
         text += " and %s day" % day
 
-text += " since the MVA was created and %d years since the last major update. A lot has changed since then." % (today.year - updated.year)
+text += " since the MVA was created and %d years since the last major update." % (today.year - updated.year)
+text += "\n%s" % choice(suffix)
 print("::set-output name=date::", text) 
 print(text)
  
