0fff895080b234ad5a5dd933ca6bc74504251d29
Andy McKay <andymckay@github.com>
2021-04-15 21:15:28 -0700

    Update message.py

--- .github/workflows/message.py
+++ .github/workflows/message.py
@@ -6,8 +6,14 @@ suffix = [
     "We are in a climate emergency.",
     "It should be the Road User Act.",
     "E-bikes are now a thing.",
-    "E-scooters and now a thing.",
+    "E-scooters are now a thing.",
     "One more lane won't fix it.",
+    "More cars aren't the answer."
+    "There were 235 fatal crashes in 2019.",
+    "There were 283 fatal crashes in 2018.",
+    "There were 264 fatal crashes in 2017.",
+    "There were 274 fatal crashes in 2016.",
+    "There were 277 fatal crashes in 2015.",    
 ]
 
 # I don't know when in 1957 the BC MVA was written.
